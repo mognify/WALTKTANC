4585102e8e106aaa71e8b6f3e6ce6713ebc3587b
Levi <levimogford@gmail.com>
2019-07-01 09:01:59 -0500

    Update TicketSelector.java

--- TicketSelector.java
+++ TicketSelector.java
@@ -1,7 +1,29 @@
-public TicketSelector{
-  public static void main(String[] args){
-  
-  }
-  
-  
+public TicketSelector {
+ public static void main(String[] args) {
+
+  System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+  Mat source = null;
+  Mat template = null;
+  String filePath = "System.getProperty("user.home") + "/Desktop\\Object Detection\\Template Matching\\Sample Image\\";
+  //Load image file
+  source = Imgcodecs.imread(filePath + "kapadokya.jpg");
+  template = Imgcodecs.imread(filePath + "balon.jpg");
+
+  Mat outputImage = new Mat();
+  int machMethod = Imgproc.TM_CCOEFF;
+  //Template matching method
+  Imgproc.matchTemplate(source, template, outputImage, machMethod);
+
+
+  MinMaxLocResult mmr = Core.minMaxLoc(outputImage);
+  Point matchLoc = mmr.maxLoc;
+  //Draw rectangle on result image
+  Imgproc.rectangle(source, matchLoc, new Point(matchLoc.x + template.cols(),
+   matchLoc.y + template.rows()), new Scalar(255, 255, 255));
+
+  Imgcodecs.imwrite(filePath + "sonuc.jpg", source);
+  System.out.println("Complated.");
+ }
+
+
 }
